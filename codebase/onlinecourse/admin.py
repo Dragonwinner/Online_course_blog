faad92e1219e94cf9e19168f9e68021aa63d20c9
Jitendra <solankijitendra032@gmail.com>
2025-03-29 14:13:49 +0530

    data

--- codebase/onlinecourse/admin.py
+++ codebase/onlinecourse/admin.py
@@ -4,22 +4,25 @@ from .models import Course, Lesson, Instructor, Learner
 
 class LessonInline(admin.StackedInline):
     model = Lesson
-    extra = 5
-
-
+    extra = 10
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
+    inlines = [LessonInline]
 
 
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
+class InstructorAdmin(admin.ModelAdmin):
+    fields = ['user', 'full_time', 'total_learners']
+    list_display = ['user', 'full_time', 'total_learners']
+
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
-admin.site.register(Instructor)
+admin.site.register(Instructor , InstructorAdmin)
 admin.site.register(Learner)
